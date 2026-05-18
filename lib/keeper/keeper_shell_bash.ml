d962c91c22755e7592a5c93ab367fd9526fbfdeb
jeong-sik <yousleepwhen@gmail.com>
2026-05-19 01:26:47 +0900

    refactor: split keeper bash shell ir classifier

--- lib/keeper/keeper_shell_bash.ml
+++ lib/keeper/keeper_shell_bash.ml
@@ -1,6 +1,7 @@
 open Keeper_types
 open Keeper_exec_shared
 open Keeper_shell_bash_redirects
+open Keeper_shell_bash_shape_ir
 open Keeper_shell_bash_shape_messages
 open Keeper_shell_bash_task_state
 open Keeper_shell_bash_words
@@ -225,27 +226,6 @@ let raw_keeper_bash_shape_block cmd =
     string_contains_substring scan_text "$(" || string_contains_char scan_text '`'
   then Some Substitution
   else None
-
-let arg_text = function
-  | Masc_exec.Shell_ir.Lit text -> Some (String.lowercase_ascii text)
-  | Masc_exec.Shell_ir.Var _ | Masc_exec.Shell_ir.Concat _ -> None
-
-let simple_is_gh_pr_checks (simple : Masc_exec.Shell_ir.simple) =
-  match Masc_exec.Bin.known simple.bin, simple.args with
-  | Some Masc_exec.Bin.Gh, arg_pr :: arg_checks :: _ ->
-    (match arg_text arg_pr, arg_text arg_checks with
-     | Some "pr", Some "checks" -> true
-     | _ -> false)
-  | _ -> false
-
-let rec parsed_keeper_bash_shape_block = function
-  | Masc_exec.Shell_ir.Pipeline _ -> Some Pipe_or_redirect
-  | Masc_exec.Shell_ir.Simple simple ->
-    if simple.redirects <> []
-    then Some Pipe_or_redirect
-    else if simple_is_gh_pr_checks simple
-    then Some Gh_pr_checks
-    else None
 
 let keeper_bash_shape_block cmd =
   let cmd, _ = strip_stderr_dev_null_redirects cmd in

992478b4982c1296bccd7a07c85059512b870048
jeong-sik <yousleepwhen@gmail.com>
2026-05-18 19:58:07 +0900

    refactor: split keeper runtime trace summaries

---
+++ lib/server/server_dashboard_http_keeper_runtime_trace_summaries.mli
@@ -0,0 +1,11 @@
+(** Provider-attempt and turn-identity summaries for runtime trace responses. *)
+
+val provider_attempts_summary_json :
+  Server_dashboard_http_keeper_runtime_manifest_scan.runtime_manifest_scan ->
+  Yojson.Safe.t
+
+val turn_identity_summary_json :
+  ?turn_id:int ->
+  Server_dashboard_http_keeper_runtime_manifest_scan.runtime_manifest_scan ->
+  Yojson.Safe.t list ->
+  Yojson.Safe.t

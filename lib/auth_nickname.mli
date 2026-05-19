d1542e3753017fe20e6fc7e703fad3bd25daa7cb
Jeong-Sik Yun <45959991+jeong-sik@users.noreply.github.com>
2026-05-19 18:07:48 +0900

    refactor: split auth nickname helpers (#16421)

---
+++ lib/auth_nickname.mli
@@ -0,0 +1,5 @@
+val trim_nonempty : string -> string option
+val is_generated_nickname_shape : string -> bool
+val keeper_transport_alias_stable_name : string -> string option
+val extract_agent_type_prefix : string -> string option
+val credential_agent_name : string -> string

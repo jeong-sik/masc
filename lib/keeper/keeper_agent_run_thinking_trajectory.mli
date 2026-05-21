6f3bc652ea1ee03097bf13e1f5f484a2b374ed10
jeong-sik <yousleepwhen@gmail.com>
2026-05-21 22:53:34 +0900

    fix thinking trajectory content type

--- lib/keeper/keeper_agent_run_thinking_trajectory.mli
+++ lib/keeper/keeper_agent_run_thinking_trajectory.mli
@@ -1,5 +1,5 @@
 val persist_response_content
   :  keeper_name:string
   -> trajectory_acc:Trajectory.accumulator option
-  -> Agent_sdk.Types.content list
+  -> Agent_sdk.Types.content_block list
   -> unit

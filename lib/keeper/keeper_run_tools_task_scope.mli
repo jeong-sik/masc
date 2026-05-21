1ebb993970343757f00f2f029151eb42e167ee2e
jeong-sik <yousleepwhen@gmail.com>
2026-05-21 21:59:18 +0900

    refactor(keeper_run_tools): move task scope parsing

--- lib/keeper/keeper_run_tools_task_scope.mli
+++ lib/keeper/keeper_run_tools_task_scope.mli
@@ -5,3 +5,13 @@ val json_string_opt : string -> Yojson.Safe.t -> string option
 
 val task_id_scope_of_tool_input :
   tool_name:string -> Yojson.Safe.t -> string option
+
+val task_id_scope_of_claim_output :
+  tool_name:string -> string -> string option
+
+val task_id_scope_of_tool_call :
+  tool_name:string ->
+  input:Yojson.Safe.t ->
+  output_text:string ->
+  meta:Keeper_types.keeper_meta ->
+  string option

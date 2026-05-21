62d816547dd4f8f6b491e17596a5b3ac64378c99
jeong-sik <yousleepwhen@gmail.com>
2026-05-21 23:30:32 +0900

    fix(keeper): repair extracted mli signatures

--- lib/keeper/keeper_heartbeat_loop_persist_cursor.mli
+++ lib/keeper/keeper_heartbeat_loop_persist_cursor.mli
@@ -3,5 +3,5 @@
 val persist_message_cursor_updates :
   config:Coord.config ->
   Keeper_types.keeper_meta ->
-  Keeper_world_observation.message_cursor_update list ->
+  (string * int) list ->
   Keeper_types.keeper_meta

8ea9c86b483126ce90b2c5f56670569f9ea00726
Jeong-Sik Yun <45959991+jeong-sik@users.noreply.github.com>
2026-05-22 12:54:43 +0900

    chore: remove snapshot view parser wrapper (#17588)

--- lib/operator/operator_control_snapshot_view.mli
+++ lib/operator/operator_control_snapshot_view.mli
@@ -15,6 +15,3 @@ val valid_snapshot_view_strings : string list
 
 val snapshot_view_of_string_opt : string -> snapshot_view option
 (** Parse a canonical view string, returning [None] for unknown inputs. *)
-
-val parse_snapshot_view : string option -> snapshot_view
-(** Back-compatible parser that falls back to [Full]. *)

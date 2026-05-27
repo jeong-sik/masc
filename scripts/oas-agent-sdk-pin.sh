394c607fce0185b85557659f53f701383293872b
jeong-sik <yousleepwhen@gmail.com>
2026-05-27 16:01:58 +0900

    chore(oas): require agent_sdk 0.200.2

--- scripts/oas-agent-sdk-pin.sh
+++ scripts/oas-agent-sdk-pin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 readonly OAS_AGENT_SDK_URL="https://github.com/jeong-sik/oas.git"
-readonly OAS_AGENT_SDK_BASE_VERSION="v0.198.5"
+readonly OAS_AGENT_SDK_BASE_VERSION="v0.200.2"
 readonly OAS_AGENT_SDK_TRACK_REF="main"
-readonly OAS_AGENT_SDK_SHA="c6b42429fc25b1e82f3b7e169b7905475665f579"
-readonly OAS_AGENT_SDK_MIN_VERSION="0.198.5"
+readonly OAS_AGENT_SDK_SHA="65b288b12a586f16936a2bee715a9aa282cbf6e3"
+readonly OAS_AGENT_SDK_MIN_VERSION="0.200.2"

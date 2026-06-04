8e1cef7da4b32065e6befe5c8cacdd0b21f87b63
jeong-sik <yousleepwhen@gmail.com>
2026-06-04 18:12:26 +0900

    keeper_cwd_response: extract Local|Sandboxed variant with of_sandbox helper

--- lib/keeper/keeper_cwd_response.ml
+++ lib/keeper/keeper_cwd_response.ml
@@ -7,12 +7,21 @@ let local ~host_cwd = Local { abs = host_cwd }
 let docker ~host_cwd ~container_cwd =
   Sandboxed { host_abs = host_cwd; container_abs = container_cwd }
 
+let init ~backend ~host_cwd ~container_cwd_for_docker =
+  match backend with
+  | "local" -> local ~host_cwd
+  | "docker" ->
+    docker ~host_cwd ~container_cwd:container_cwd_for_docker
+  | _ ->
+    Log.Keeper.warn (fun m ->
+      m "Keeper_cwd_response.init: unknown backend %S, falling back to local"
+        backend);
+    local ~host_cwd
+
 let of_sandbox ~(sandbox : Keeper_sandbox.t) ~host_cwd
     ~container_cwd_for_docker =
-  match sandbox.backend with
-  | Keeper_sandbox.Local -> local ~host_cwd
-  | Keeper_sandbox.Docker ->
-    docker ~host_cwd ~container_cwd:container_cwd_for_docker
+  init ~backend:(Keeper_sandbox.backend_name sandbox) ~host_cwd
+    ~container_cwd_for_docker
 
 let keeper_visible = function
   | Local { abs } -> abs

c328b83d555dbd95cb273a13310048557ec383c3
jeong-sik <yousleepwhen@gmail.com>
2026-06-04 16:57:49 +0900

    fix: expose execute schema helper interface

--- lib/tool_surface/tool_shard_types_schemas_execute.mli
+++ lib/tool_surface/tool_shard_types_schemas_execute.mli
@@ -1,4 +1,5 @@
 (** Typed Execute tool schemas. *)
 
+val tool_execute_timeout_sec_field : string * Yojson.Safe.t
 val tool_execute_schema : Masc_domain.tool_schema
 val typed_execute_tools : Masc_domain.tool_schema list

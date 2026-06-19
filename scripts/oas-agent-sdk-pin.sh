a30b194427763417627145527178fee74db68e86
jeong-sik <yousleepwhen@gmail.com>
2026-06-19 18:25:53 +0900

    chore(oas): bump agent sdk pin to 0.207.5

--- scripts/oas-agent-sdk-pin.sh
+++ scripts/oas-agent-sdk-pin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 readonly OAS_AGENT_SDK_URL="https://github.com/jeong-sik/oas.git"
-readonly OAS_AGENT_SDK_BASE_VERSION="v0.207.3"
+readonly OAS_AGENT_SDK_BASE_VERSION="v0.207.5"
 readonly OAS_AGENT_SDK_TRACK_REF="main"
-readonly OAS_AGENT_SDK_SHA="57ed7272499e8c8359eb344244bd769e05f0bd40"
-readonly OAS_AGENT_SDK_MIN_VERSION="0.207.3"
+readonly OAS_AGENT_SDK_SHA="6fe842bf2fdb869b2ed9004029f7cdfd7cd7ca5e"
+readonly OAS_AGENT_SDK_MIN_VERSION="0.207.5"

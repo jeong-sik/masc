40b9c0b1535f797913a275dd5cee546fa2fde29d
nick0cave <nick0cave@masc.local>
2026-07-09 16:39:54 +0100

    feat: add Operator_blocked task state with Mark_operator_blocked/Unblock transitions

--- lib/server/server_dashboard_http.ml
+++ lib/server/server_dashboard_http.ml
@@ -589,7 +589,7 @@ let dashboard_planning_http_json ~(config : Workspace.config) : Yojson.Safe.t =
             match task.task_status with
             | Todo -> todo + 1, claimed, running, done_count, cancelled
             | Claimed _ -> todo, claimed + 1, running, done_count, cancelled
-            | InProgress _ | AwaitingVerification _ ->
+            | InProgress _ | AwaitingVerification _ | Operator_blocked _ ->
               todo, claimed, running + 1, done_count, cancelled
             | Done _ -> todo, claimed, running, done_count + 1, cancelled
             | Cancelled _ -> todo, claimed, running, done_count, cancelled + 1)

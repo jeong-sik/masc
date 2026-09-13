f4f279f99b628ed25aa3efe61de45f298b8c58b1
jeong-sik <yousleepwhen@gmail.com>
2026-09-14 00:30:33 +0900

    fix(keeper): align agent continuation reference signature order

--- lib/keeper/keeper_agent_run.mli
+++ lib/keeper/keeper_agent_run.mli
@@ -263,8 +263,8 @@ val run_turn
   -> ?degraded_retry_runtime:string
   -> ?fallback_reason:Keeper_error_classify.degraded_retry_reason
   -> ?runtime_rotation_attempts:Keeper_execution_receipt.runtime_rotation_attempt list
-  -> ?official_task_reference:Keeper_official_task_reference.t
   -> ?direct_resume:direct_continuation
+  -> ?official_task_reference:Keeper_official_task_reference.t
   -> ?on_gate_evidence_admitted:(Agent_core.Checkpoint.t -> (unit, string) result)
   -> ?deferred_runtime_lane:Keeper_turn_driver.deferred_runtime_lane
   -> ?on_runtime_retry_deferred:

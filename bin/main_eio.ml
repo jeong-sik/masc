5e8edd55b7869152482fcbb1c4e28e7999fef17d
jeong-sik <yousleepwhen@gmail.com>
2026-09-25 02:13:56 +0900

    fix: derive OCI version label from recipe build

--- bin/main_eio.ml
+++ bin/main_eio.ml
@@ -3170,12 +3170,10 @@ let sandbox_image_cmd_exit print_only tag runtime recipe_name source =
       let* runtime = runtime in
       let* builder = sandbox_image_builder runtime in
       let built_at = Unix.gettimeofday () in
-      let tag, version =
+      let tag =
         match tag with
-        | Some tag -> tag, tag
-        | None ->
-          ( Keeper_sandbox_image_version.tag ~built_at recipe
-          , Keeper_sandbox_image_version.version ~built_at recipe )
+        | Some tag -> tag
+        | None -> Keeper_sandbox_image_version.tag ~built_at recipe
       in
       match sandbox_image_tag_presence ~command:builder.build_command ~tag with
       | Store_unanswered detail ->
@@ -3193,7 +3191,7 @@ let sandbox_image_cmd_exit print_only tag runtime recipe_name source =
               image first."
              tag)
       | Tag_absent ->
-        let labels = Keeper_sandbox_image_version.labels ~version ~built_at recipe in
+        let labels = Keeper_sandbox_image_version.labels ~built_at recipe in
         Ok (sandbox_image_build ~builder ~recipe ~tag ~labels)
   in
   match run with
@@ -3225,7 +3223,7 @@ let sandbox_image_ensure_exit runtime =
      | Tag_absent ->
        let recipe = Keeper_sandbox_image_version.base_embedded in
        let built_at = Unix.gettimeofday () in
-       let labels = Keeper_sandbox_image_version.labels ~version:tag ~built_at recipe in
+       let labels = Keeper_sandbox_image_version.labels ~built_at recipe in
        sandbox_image_build ~builder ~recipe ~tag ~labels)
 
 let sandbox_image_cmd =
